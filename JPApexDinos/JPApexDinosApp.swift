2bfc9a149412c047c39422f4b196fce2f79cb475
LubenZA <lubenivanchev@gmail.com>
2025-05-11 16:02:38 +0200

    Initial Commit

---
+++ JPApexDinos/JPApexDinosApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexDinosApp.swift
+//  JPApexDinos
+//
+//  Created by Luben Ivanchev on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct JPApexDinosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
